35e78b64c2f3f67dda5a1b67b0ca55be61fcda29
Marco <marco.brighi4@studio.unibo.it>
2018-04-26 10:24:21 +0200

    Add a doc inside MyJavaClass

--- src/main/java/MyJavaClass.java
+++ src/main/java/MyJavaClass.java
@@ -2,8 +2,13 @@ package main.java;
 
 public class MyJavaClass {
 
+    /*
+     * Add doc about this difficult method
+     */
     public static void main(String... args) {
+
         System.out.println("This is my Java class!");
+
     }
 
 
